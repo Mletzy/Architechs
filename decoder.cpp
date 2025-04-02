3601c3b62520ed251d286306bb27b9395725aba2
gm21 <67883963+gmaddox21@users.noreply.github.com>
2025-04-01 22:03:50 -0500

    Spelling

--- decoder.cpp
+++ decoder.cpp
@@ -6,7 +6,7 @@ std::vector<Instruction> Decoder::decodeAll(const std::vector<std::string> &bina
     std::vector<Instruction> result; // Vector to store decoded instructions
     int idx = 1;                     // Instruction index
 
-    // Loop through each binary string and create a decoded Instrucction object
+    // Loop through each binary string and create a decoded Instruction object
     for (const auto &binary : binaryList)
     {
         result.emplace_back(binary, idx++); // Emplace = contruct in-place
